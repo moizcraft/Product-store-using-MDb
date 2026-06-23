bd57033515503ed3aedac7892abeb1fdabcf6dc1
Abdul-Moiz1 <abdulmoiz.codes@gmail.com>
2026-06-24 02:41:57 +0500

    update mongoDB url

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async () => {
-    await mongoose.connect(`mongodb+srv://${process.env.DbUserName}:${process.env.DbPassword}@cluster3.m0d4qqk.mongodb.net/${process.env.DbName}`)
+    await mongoose.connect(process.env.MONGODB_URI);
 }
 
 
