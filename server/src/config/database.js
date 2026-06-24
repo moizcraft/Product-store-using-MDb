454670c68c55443cc9c394b3f558d62ff7a674cf
Abdul-Moiz1 <abdulmoiz.codes@gmail.com>
2026-06-24 14:53:50 +0500

    check mongdodb

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI);
-}
+    try {
+        console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log('MongoDB Connected Successfully');
+    } catch (error) {
+        console.error('Database connection failed', error);
+        process.exit(1);
+    }
+}
 
 module.exports = connectDB;
