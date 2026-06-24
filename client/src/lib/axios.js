f06496df8181b0f8339b2a72be178d7cde892cbd
Abdul-Moiz1 <abdulmoiz.codes@gmail.com>
2026-06-24 15:14:09 +0500

    add backend URL

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://product-store-using-mdb-production.up.railway.app/",
   withCredentials: true,
   timeout: 10000, // 10 second timeout
 });
